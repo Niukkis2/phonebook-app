55177286fcc44a67a5e38ba289eeefe1b0b5f1d0
Niukkis <basanaama@gmail.com>
2020-10-27 12:16:28 +0200

    backend working

--- index.js
+++ index.js
@@ -1,30 +1,89 @@
-const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
-let notes = [
-    {    id: 1,    
-        content: "HTML is easy",    
-        date: "2019-05-30T17:30:31.098Z",    
-        important: true  
+app.use(express.json())
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+    skip: (req, res) => {
+        return req.method.localeCompare('POST') != 0
+    }
+}))
+
+let persons = [
+    {    
+        id: 1,    
+        name: "Arto Hellas",    
+        number: "040-123456"
     },  
     {    
         id: 2,    
-        content: "Browser can execute only Javascript", 
-        date: "2019-05-30T18:39:34.091Z",   
-        important: false
+        name: "Ada Lovelace",    
+        number: "39-44-5323523"
     },  
     {    
-        id: 3,   
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2019-05-30T19:20:14.298Z",    
-        important: true  
+        id: 3,    
+        name: "Dan Abramov",    
+        number: "12-43-234345"
+    },
+    {
+        id: 4,    
+        name: "Mary Poppendieck",    
+        number: "39-23-6423122"
     }
 ]
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'application/json' });
-    res.end(JSON.stringify(notes));
-});
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>')
+})
 
-const PORT = 3001;
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    person === undefined ? res.status(404).end() : res.json(person)
+})
+
+app.get('/info', (req, res) => {
+    const dateDisplay = new Date()
+    res.send("<h3>Phonebook has info for " + persons.length + " people</h3>" + 
+             "<h3>" + dateDisplay + "</h3>")
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    const generateId = () => {
+        return Math.floor(Math.random() * (100000 - 1) + 1)
+    }
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name and/or number missing'
+        })
+    }
+    if (persons.find(p => p.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
