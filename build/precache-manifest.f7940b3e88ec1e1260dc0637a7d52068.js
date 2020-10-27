94064bd1f52d81e09e2dffb48285ab066f4d8057
Niukkis <basanaama@gmail.com>
2020-10-27 12:47:35 +0200

    uibuild

---
+++ build/precache-manifest.f7940b3e88ec1e1260dc0637a7d52068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f56b00b88f790dc6b22c8a17214026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "065db46f88392dc27583",
+    "url": "/static/css/main.f246bc0c.chunk.css"
+  },
+  {
+    "revision": "ea4e20ee09e0ce253014",
+    "url": "/static/js/2.134ea264.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.134ea264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065db46f88392dc27583",
+    "url": "/static/js/main.2bfff189.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
