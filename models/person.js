676e36b5d6a21e56914a1f00c2ccbd82e58f0ef8
Niukkis <basanaama@gmail.com>
2020-10-30 13:17:17 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-const url = process.env.MONGO_DB_URI
+const url = process.env.MONGODB_URI
 
 console.log('connecting to: ', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
